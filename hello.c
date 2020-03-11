7e1801caa328aefe427d462710dc98f99e70849c
guoliao <guoliao@kaochong.com>
2020-03-11 15:28:14 +0800

    C

--- hello.c
+++ hello.c
@@ -27,13 +27,13 @@
 * long double：16字节
 * %E 为以指数形式输出单、双精度实数
 */
-// #include <stdio.h>
-// #include <float.h>
-// int main()
-// {
-//     printf("float 存储最大字节数：%lu \n", sizeof(float));
-//     printf("float 存储最大值：%E \n", FLT_MAX);
-//     printf("float 存储最小值：%E \n", FLT_MIN);
-//     printf("float 存储精度：%E \n", FLT_DIG);
-//     return 0;
-// }
+#include <stdio.h>
+#include <float.h>
+int main()
+{
+    printf("float 存储最大字节数：%lu \n", sizeof(float));
+    printf("float 存储最大值：%E \n", FLT_MAX);
+    printf("float 存储最小值：%E \n", FLT_MIN);
+    printf("float 存储精度：%E \n", FLT_DIG);
+    return 0;
+}
---
+++ one/one-1.c
@@ -0,0 +1,30 @@
+
+// 自定义类型typedef
+// 创建变量TYPE name:
+// 通过打印证明本质printf
+
+#include <stdio.h>
+
+typedef int INT32;
+typedef unsigned char BYTE;
+typedef struct _demo
+{
+    short s;
+    BYTE b1;
+    BYTE b2;
+    INT32 i;
+} DEMO;
+
+int main()
+{
+    INT32 i32;
+    BYTE byte;
+    DEMO d;
+    printf("%d, %d\n", sizeof(INT32), sizeof(i32));
+    printf("%d, %d\n", sizeof(BYTE), sizeof(byte));
+    printf("%d, %d\n", sizeof(DEMO), sizeof(d));
+    return 0;
+}
+// 4, 4
+// 1, 1
+// 8, 8
