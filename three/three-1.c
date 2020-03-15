ab65a0ceb6aef3612662f71f6fe3009312e1a288
guoliao <guoliao@kaochong.com>
2020-03-15 10:56:31 +0800

    c

---
+++ three/three-1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+typedef enum _bool
+{
+    FALSE,
+    TRUE,
+} BOOL;
+int main()
+{
+    int i32;
+    char byte;
+    short d;
+    BOOL b;
+    if(0 == i32)
+    {
+        i32++;
+    } 
+    else if (0 == byte)
+    {
+        byte++;
+    }
+    else if (0 == d)
+    {
+        d++;
+    } 
+    else
+    {
+        printf("Error");
+    }
+    
+}
