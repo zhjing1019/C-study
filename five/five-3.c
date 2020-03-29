be6881ef08e82d5e837d74c45b2e7feed5eb2bdf
guoliao <guoliao@kaochong.com>
2020-03-29 16:11:26 +0800

    C

---
+++ five/five-3.c
@@ -0,0 +1,68 @@
+// C语言存储斐波那契数列
+#include <stdio.h>
+#include <malloc.h>
+typedef struct _soft_array
+{
+    int len;
+    int array[];
+}SoftArray;
+
+SoftArray* create_soft_array(int size)
+{
+    SoftArray* ret = NULL;
+    if(size > 0)
+    {
+        ret = (SoftArray*)malloc(sizeof(*ret) + sizeof(*(ret->array)) * size);
+        ret->len = size;
+    }
+    return ret;
+}
+// 生成斐波那契数列
+void fac(SoftArray* sa) 
+{
+    int i = 0;
+    if(NULL != sa) {
+        if(1 == sa->len)
+        {
+            sa->array[0] = 1;
+        }
+        else if( 2 == sa->len)
+        {
+            sa->array[0] = 1;
+            sa->array[1] = 1;
+        }
+        else 
+        {
+            sa->array[0] = 1;
+            sa->array[1] = 1;
+            for(i = 2; i<sa->len; i++ )
+            {
+                sa->array[i] = sa->array[i - 1] + sa->array[i-2];
+            }
+
+        }
+    }
+    
+
+}
+
+void delete_soft_array(SoftArray* sa)
+{
+    free(sa);
+}
+
+int main()
+{
+    int i =0;
+
+    SoftArray* sa = create_soft_array(10);
+    printf("%d\n", sa->len);
+    fac(sa);
+
+    for(i = 0; i<sa->len; i++)
+    {
+        printf("%d\n", sa->array[i]);
+    }
+    delete_soft_array(sa);
+    return 0;
+}
