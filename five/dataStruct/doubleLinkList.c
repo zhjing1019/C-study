78a81408b5417f377ef2b29fc4c1d27e365462b8
张静 <zhjing1019@gmail.com>
2020-07-28 11:40:37 +0800

    双链表

--- five/dataStruct/doubleLinkList.c
+++ five/dataStruct/doubleLinkList.c
@@ -11,15 +11,22 @@ typedef struct DNode {
 void InitDouList(DouLinkList &D) {
     D = (DNode *)malloc(sizeof(DNode));
     if(D == NULL)
-        return false;
-
+        return ;
     D->data = 0;
     D->prior = NULL;
     D->next = NULL;
-}
-
+};
+bool InsertDouList(DNode *p, DNode *s) {
+    s->next = p->next;
+    p->next->prior = s;
+    s->prior = p;
+    p->next = s;
+    return true;
+};
 
 int main() {
     DouLinkList L;
     InitDouList(L);
-}
+    DouLinkList s;
+    InsertDouList(L, s);
+};
