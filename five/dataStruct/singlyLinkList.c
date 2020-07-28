f17864dc1617e65a319f18cf358f215004606049
张静 <zhjing1019@gmail.com>
2020-07-28 10:58:44 +0800

    尾插法建立单链表

---
+++ five/dataStruct/singlyLinkList.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct LNode {
+    int data;
+    struct LNode *next;
+}SqList, *LinkList;
+
+// 尾插法建立单链表
+bool InsertNextNode(LinkList &L, int i, int e) {
+    if(i < 1) 
+        return false;
+    LNode *p;
+    int j = 0;
+    while(p != NULL && j < i - 1) {
+        p = p->next;
+        j++;
+    }
+    if(p == NULL) {
+        return false;
+    }
+    LNode *s = (LNode *)malloc(sizeof(LNode));
+    s->next = p->next;
+    s->data = e;
+    p->next = s;
+    return true;
+}
+int main() {
+    LinkList L;
+    int e = 3;
+    InsertNextNode(L, 6, e);
+}
