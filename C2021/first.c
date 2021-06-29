8240ca7a4c08014839d8ad4817897a3240be08fa
张静 <zhangjing@aiforward.com>
2021-06-29 14:42:31 +0800

    C

---
+++ C2021/first.c
@@ -0,0 +1,15 @@
+// 要用到输入输出函数！
+# include <stdio.h>
+// hello world C program
+int main()
+{
+  printf("hello world C program \n");
+  // 求俩个数之和
+  int a, b, sum;
+  a = 123;
+  b = 321;
+  sum = a + b;
+  printf("sum is %d\n", sum);
+  return 0;
+}
+
