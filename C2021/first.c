3291612c18b5800964c1bc15a67049aeb1a9a08e
张静 <zhangjing@aiforward.com>
2021-07-14 11:14:22 +0800

    C

--- C2021/first.c
+++ C2021/first.c
@@ -30,6 +30,24 @@ int main()
   }
   printf("%d\n", t);
 
+  //给定一个大写字母，要求用小写字母输出
+  char c1, c2;
+  c1 = 'A';
+  c2 = c1 + 32;
+  printf("%c\n", c1);
+  printf("%c\n", c2);
+
+  // 输入一个大于3的整数n，判定他是否为素数
+  int n, i;
+  printf("please enter a integer number, n=?");
+  scanf("%d", &n);
+  for(i = 2;i < n-1; i++) {
+    if(n%i == 0) break;
+    if(i < n) printf("%d is not prime number. \n", n);
+    else printf("%d is a prime number.\n", n);
+    return 0;
+  }
+
   return 0;
 }
 
